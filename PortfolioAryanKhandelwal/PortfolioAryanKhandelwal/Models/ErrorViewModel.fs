1cb1a9525ccd3e69c9e5be0721303c59a189650d
Aryan Khandelwal <arykhandelwal52@gmail.com>
2021-08-11 11:01:38 +0530

    Deleting unused files

--- PortfolioAryanKhandelwal/PortfolioAryanKhandelwal/Models/ErrorViewModel.fs
+++
@@ -1,9 +0,0 @@
-namespace PortfolioAryanKhandelwal.Models
-
-open System
-
-type ErrorViewModel =
-    { RequestId: string }
-
-    member this.ShowRequestId =
-        not (String.IsNullOrEmpty(this.RequestId))
