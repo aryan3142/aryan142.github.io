ac652ba1c8478c9c959a40d7a3293f5fdbe55194
Aryan <arykhandelwal52@gmail.com>
2022-01-18 00:14:04 +0530

    Added image url fix

--- script.js
+++ script.js
@@ -19,20 +19,20 @@ for(var i=0; i < themeDots.length; i++){
 function setTheme(mode){
     if(mode == 'light'){
         document.getElementById('theme-style').href = 'default.css';
-        document.getElementById('profile_pic').src = 'images/Aryan.png';
+        document.getElementById('profile_pic').src = 'Images/Aryan.png';
     }
     if(mode == 'blue'){
         document.getElementById('theme-style').href = 'blue.css';
-        document.getElementById('profile_pic').src = 'images/AryanBlue.jpg';
+        document.getElementById('profile_pic').src = 'Images/AryanBlue.jpg';
 
     }
     if(mode == 'green'){
         document.getElementById('theme-style').href = 'dark.css';
-        document.getElementById('profile_pic').src = 'images/AryanDark.jpg';
+        document.getElementById('profile_pic').src = 'Images/AryanDark.jpg';
     }
     if(mode == 'purple'){
         document.getElementById('theme-style').href = 'purple.css';
-        document.getElementById('profile_pic').src = 'images/AryanPurple.jpg';
+        document.getElementById('profile_pic').src = 'Images/AryanPurple.jpg';
     }
 
     localStorage.setItem('theme', mode);
